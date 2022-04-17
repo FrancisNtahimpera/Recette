71b998cb2b6aed0d7311924b840d40ab38ab3464
Francisntahimpera <ntahimperafrancis@gmail.com>
2022-04-17 12:50:29 +0200

    setup model and relationship withPivot request

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Recipe;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Ingredient extends Model
 {
     use HasFactory;
+
+    
+    public function recipes():  BelongsToMany
+    { 
+    
+        return $this->belongsToMany(Recipe::class)
+        ->withPivot(['amount' , 'unit']);
+ 
+    }
 }
