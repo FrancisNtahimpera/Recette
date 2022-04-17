6946abd94b6f0cf8b320c38163c89331f47d4c00
Francisntahimpera <ntahimperafrancis@gmail.com>
2022-04-17 12:40:12 +0200

    setup factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Recipe;
+use App\Models\Ingredient;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(20)->has(
+            Recipe::factory(3)->hasAttached(
+                Ingredient::factory(5), 
+                [
+                    'amount'=> 10,
+                    'unit'=> 'cl'
+                               
+                ]
+            )
+        )
+        
+        
+        
+        
+        
+        ->create();
     }
 }
