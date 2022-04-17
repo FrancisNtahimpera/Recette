d0ae76e64136a48d7351a37543a35d1300e2fddb
Francisntahimpera <ntahimperafrancis@gmail.com>
2022-04-17 13:12:38 +0200

    make migration , seeder ok

--- database/migrations/2022_04_17_102125_create_ingredient_recipe.php
+++ database/migrations/2022_04_17_102125_create_ingredient_recipe.php
@@ -16,9 +16,9 @@ return new class extends Migration
         // table pivot 
 
         Schema::create('ingredient_recipe', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('ingredient_id');
-            $table->unsignedBigInteger('recipe_id');
+            
+           // $table->unsignedBigInteger('ingredient_id');
+           // $table->unsignedBigInteger('recipe_id');
             
             $table->unsignedBigInteger('ingredient_id');
             $table->foreign('ingredient_id')->references('id')->on('ingredients');
